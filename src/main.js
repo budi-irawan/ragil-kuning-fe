6a5eb0bc433d826b5645374eb00c1dfd41b4c411
budi-irawan <budi.irawan171717@gmail.com>
2023-06-23 07:59:30 +0700

    login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
 import 'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'
@@ -11,6 +12,11 @@ import './style.scss'
 import 'admin-lte/plugins/jquery/jquery.min.js'
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js'
 import 'admin-lte/dist/js/adminlte.min.js?v=3.2.0'
+
+var eventBus = new Vue();
+Vue.prototype.$eventBus = eventBus
+
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
