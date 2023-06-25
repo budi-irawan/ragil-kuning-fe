c550c5daedb9de5e3e8f29aaf017246e851b5904
budi-irawan <budi.irawan171717@gmail.com>
2023-06-25 15:43:59 +0700

    list

--- src/components/partials/AppNavBar.vue
+++ src/components/partials/AppNavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="main-header navbar navbar-expand navbar-dark shadow-sm">
-    <ul v-if="!isKasir" class="navbar-nav">
+    <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"
           ><i class="fas fa-bars"></i
@@ -42,7 +42,6 @@ export default {
       return this.item_user && this.item_user.role === "ADMIN";
     },
     isKasir() {
-      console.log(this.item_user);
       return this.item_user && this.item_user.role === "KASIR";
     },
   },
