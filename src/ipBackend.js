81e56481fd43fa7858d46c94b66a66a86fbec6d1
budi-irawan <budi.irawan171717@gmail.com>
2023-09-01 11:00:18 +0700

    ip

--- src/ipBackend.js
+++ src/ipBackend.js
@@ -1,4 +1,4 @@
-export const ipBackend = "http://localhost:3000"
+export const ipBackend = "http://localhost:3001"
 // export const ipBackend = "https://192.168.43.117:3000"
 // export const ipBackend = "https://45.126.187.15:3001"
 // export const ipBackend = "http://192.168.99.100:4000"
