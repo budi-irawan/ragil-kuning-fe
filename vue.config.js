4ad7a21260d533601357915de0494b894793207a
budi-irawan <budi.irawan171717@gmail.com>
2023-09-14 23:40:45 +0700

    path

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/dist/'
-  : '/',
+  publicPath: './',
   transpileDependencies: true,
   pages: {
     index: {
       entry: 'src/main.js',
       title: 'RAGIL KUNING',
     },
-  },
-  devServer: {
-    proxy: 'https://192.168.99.100:4000'
   },
 })
